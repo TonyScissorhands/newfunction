aeb2c3c2622201cea00ed3a62fb9a2b172f1b49e
lichen <lc460620829@163.com>
2021-08-06 11:09:06 +0800

    'init'

---
+++ src/newfunction.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by
+ * User: lichen
+ * Date: 2021/8/6
+ * TIME: 10:50
+ */
+
+namespace scissorhands;
+
+
+class newfunction
+{
+    public function test()
+    {
+        return 111111111;
+    }
+}
